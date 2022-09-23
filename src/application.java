2fec6965d2f2a2dc517010a5a7fa6499f0876801
Ally Rally <110881902+cloneboy99@users.noreply.github.com>
2022-09-22 23:39:23 -0700

    half way there

--- src/application.java
+++ src/application.java
@@ -8,27 +8,35 @@ public class application {
       3, 9, 23, 64, 2, 8, 28, 93, 3
       };
     
-    int lastMinusFirst = ages[ages.length - 1] - ages[0];
-    System.out.println(lastMinusFirst);
+   // int lastMinusFirst = ages[ages.length - 1] - ages[0];
+   // System.out.println(lastMinusFirst);
   
     int sumOfAges = 0;
        for(int i = 1; i < ages.length; i++) {
       sumOfAges = sumOfAges + ages[i];
     }
     int averageOfAges = sumOfAges / ages.length;
-       System.out.println(averageOfAges);
+       System.out.println("Average age: " + averageOfAges);
    
     
     String[] names = new String[] {
         "Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"
     };
-    
-    for(int i = 0; i < names.length; i++) {
-      long sumOfNames = 0;
-      long sumOfNames = sumOfNames + names[i].chars().count();
+    int i = 0;
+    long sumOfNames = 0;
+    while(i < names.length){
+      sumOfNames = sumOfNames + names[i].chars().count();
+      i++;
     }
     long avgOfNames = sumOfNames / names.length;
-    System.out.println(avgOfNames);
+    System.out.println("Average name length: " + avgOfNames + " characters");
+  
+    int index = 0;
+    while(index < names.length) {
+      System.out.printf(names[index] + " ");
+      index++;
+    }
+    System.out.println();
   }
 }
 
